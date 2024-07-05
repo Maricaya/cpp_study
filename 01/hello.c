5948aed047b07917622db887bfe16e5277b02e86
Maricaya <915270549@qq.com>
2024-07-05 10:46:26 -0500

    feat: day1

---
+++ 01/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define PI 3.14
+
+int main(void)
+{
+  // int a = 1;
+  int a = 1;
+  // int b = 3;
+  int b = 3;
+  // int c = a+b;
+  int c = a+b;
+
+  //printf("hello world!\n");
+  printf("%d", c);
+  return 0;
+}
